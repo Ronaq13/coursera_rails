8e2259f12bb3529658f99a27b3da3cf87098faf0
Ronaq13 <raounaqsharma@gmail.com>
2017-03-17 14:10:42 +0530

    stuck at sharing parameters btw views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/android' => 'home#index_android'   # where all android courses listed
   get '/web' => 'home#index_web'           # where all web courses listed
   get '/analytics' => 'home#index_analytics'     # where all analytics courses listed
-  get '/course_view' => 'home#course_view_home'  # view the course when user is not sign in
+  get '/course_view_home' => 'home#course_view_home'  # view the course when user is not sign in
 end
